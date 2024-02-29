531646b14b6b58c2b97b015e275db31bb5cad354
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-02-29 20:22:23 +0000

    style: use `black` as formatter and linter

--- iaa_seminario1/__main__.py
+++ iaa_seminario1/__main__.py
@@ -1,30 +1,35 @@
-import pysmile;
-import pysmile_license;
-import typer;
-from InquirerPy import inquirer;
+import pysmile
+import pysmile_license  # noqa: 403
+import typer
+from InquirerPy import inquirer
 
-app = typer.Typer();
+app = typer.Typer()
+
 
 def evidence(net, node):
-    ids = net.get_outcome_ids(node);
-    name = net.get_node_name(node);
-    selection = inquirer.select(f"Introduce el estado del bot en \"{name}\":", choices=ids, default=None).execute();
+    ids = net.get_outcome_ids(node)
+    name = net.get_node_name(node)
+    selection = inquirer.select(
+        f'Introduce el estado del bot en "{name}":', choices=ids, default=None
+    ).execute()
     if selection in ids:
-        net.set_evidence(node, selection);
-    return net;
+        net.set_evidence(node, selection)
+    return net
+
 
 @app.command()
 def main():
-    net = pysmile.Network();
-    net.read_file("assets/ModeladoBot.xdsl");
+    net = pysmile.Network()
+    net.read_file("assets/ModeladoBot.xdsl")
     net.get_outcome_ids
-    nodes = net.get_all_nodes();
+    nodes = net.get_all_nodes()
     for node in nodes:
         if node != 1:
-            net = evidence(net, node);
+            net = evidence(net, node)
 
-    net.update_beliefs();
-    print(net.get_node_value("future_bot"));
+    net.update_beliefs()
+    print(net.get_node_value("future_bot"))
+
 
 if __name__ == "__main__":
-    app();
+    app()
