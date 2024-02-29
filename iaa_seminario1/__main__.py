f6779b73655773f3b96264f61c07324844d9295a
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-02-29 13:55:33 +0000

    feat: add pysmile proof of concept

---
+++ iaa_seminario1/__main__.py
@@ -0,0 +1,25 @@
+import pysmile;
+import pysmile_license;
+
+def evidence(net, node):
+    ids = net.get_outcome_ids(node);
+    print("Introduce el estado del bot en ", node, ":", ids);
+    selection = input();
+    if selection in ids:
+        net.set_evidence(node, selection);
+    return net;
+
+def main():
+    net = pysmile.Network();
+    net.read_file("assets/ModeladoBot.xdsl");
+
+    net.get_outcome_ids
+    nodes = net.get_all_nodes();
+    for node in nodes:
+        if node != 1:
+            net = evidence(net, node);
+
+    net.update_beliefs();
+    print(net.get_node_value("future_bot"));
+
+main();
