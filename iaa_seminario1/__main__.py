7c9bace1d4c65b7bd9394a01c37ad20b3f71079b
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-02-29 19:57:54 +0000

    feat: add typer cli

--- iaa_seminario1/__main__.py
+++ iaa_seminario1/__main__.py
@@ -1,18 +1,22 @@
 import pysmile;
 import pysmile_license;
+import typer;
+
+app = typer.Typer();
 
 def evidence(net, node):
     ids = net.get_outcome_ids(node);
-    print("Introduce el estado del bot en ", node, ":", ids);
+    name = net.get_node_name(node);
+    print(f"Introduce el estado del bot en {name}:", ids);
     selection = input();
     if selection in ids:
         net.set_evidence(node, selection);
     return net;
 
+@app.command()
 def main():
     net = pysmile.Network();
     net.read_file("assets/ModeladoBot.xdsl");
-
     net.get_outcome_ids
     nodes = net.get_all_nodes();
     for node in nodes:
@@ -23,4 +27,4 @@ def main():
     print(net.get_node_value("future_bot"));
 
 if __name__ == "__main__":
-    main();
+    app();
