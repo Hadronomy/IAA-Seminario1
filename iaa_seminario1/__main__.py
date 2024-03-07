4c566cb697e8eeb75137e2c2f29b73d41570c61b
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-03-07 21:56:17 +0000

    feat: add `tendencies` command

--- iaa_seminario1/__main__.py
+++ iaa_seminario1/__main__.py
@@ -1,10 +1,11 @@
 import pysmile
-import pysmile_license  # noqa: 403
 import typer
 import pandas as pd
 import numpy as np
 import warnings
 from InquirerPy import inquirer
+
+import iaa_seminario1.license  # noqa: 403
 
 app = typer.Typer()
 
@@ -20,10 +21,21 @@ def evidence(net, node):
     return net
 
 
+def calculate_next_state(net):
+    net.update_beliefs()
+    future_probability = np.array(net.get_node_value("future_bot"))
+    tags = np.array(net.get_outcome_ids("future_bot"))
+    dataframe = pd.DataFrame(future_probability, index=tags, columns=["Probability"])
+    sample = dataframe.sample(n=1, weights="Probability")
+    next_state = sample.index[0]
+    return dataframe, next_state
+
+
 @app.command()
-def main():
+def probabilities():
     """
     Reads the bot model and asks for the evidence of each node
+    and then calculates the probability of the next state of the bot
     """
     net = pysmile.Network()
     net.read_file("assets/ModeladoBot.xdsl")
@@ -32,19 +44,63 @@ def main():
         if node != 1:
             net = evidence(net, node)
 
-    net.update_beliefs()
-    future_probability = np.array(net.get_node_value("future_bot"))
-    tags = np.array(net.get_outcome_ids("future_bot"))
-    dataframe = pd.DataFrame(future_probability, index=tags, columns=["Probability"])
-    sample = dataframe.sample(n=1, weights="Probability")
-    next_state = sample.index[0]
+    dataframe, next_state = calculate_next_state(net)
+    print(dataframe)
     print(f"Next state of the bot: {next_state}")
 
 
-if __name__ == "__main__":
+@app.command()
+def tendencies():
+    net = pysmile.Network()
+    net.read_file("assets/ModeladoBot.xdsl")
+    states = {
+        2: "Alta",
+        3: "Armado",
+        4: "Si",
+        5: "Si",
+        6: "Si",
+        7: "Si",
+        8: "Si",
+    }
+    total_iterations = 0
+    repetitions_counter = 0
+    previous_state = None
+    states[0] = "huir"
+    while repetitions_counter < 20:
+        for node in states:
+            net.set_evidence(node, states[node])
+        dataframe, next_state = calculate_next_state(net)
+        next_current_state_map = {
+            "buscar_armas": "recoger_arma",
+            "buscar_energia": "recoger_energía",
+        }
+        next_state = (
+            next_current_state_map[next_state]
+            if next_state in next_current_state_map
+            else next_state
+        )
+        print(f"📝 Iteration: {total_iterations}")
+        print(f"Current state of the bot: {states[0]}\n")
+        print(dataframe)
+        print(f"\nNext state of the bot: {next_state}")
+        print("----------------------------------------------")
+        if previous_state == next_state:
+            repetitions_counter += 1
+        else:
+            repetitions_counter = 0
+        previous_state = next_state
+        states[0] = next_state
+        total_iterations += 1
+
+
+def main():
     with warnings.catch_warnings():
         # Supress all numpy warnings:
         # pysmile is setting the smallest_subnormal float64 value to 0
         # which is quite dumb, but we can't do anything about it
         warnings.filterwarnings("ignore", module=r"numpy")
         app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ iaa_seminario1/license/__init__.py
@@ -0,0 +1,86 @@
+# License issued by BayesFusion Licensing Server
+# The code below must be executed before any PySMILE object is created.
+# You can use "import pysmile_license" or copy the pysmile.License
+# call into your Python source code.
+import pysmile
+
+pysmile.License(
+    (
+        b"SMILE LICENSE e277e7a2 b363df6d 5300fd4e "
+        b"THIS IS AN ACADEMIC LICENSE AND CAN BE USED "
+        b"SOLELY FOR ACADEMIC RESEARCH AND TEACHING, "
+        b"AS DEFINED IN THE BAYESFUSION ACADEMIC "
+        b"SOFTWARE LICENSING AGREEMENT. "
+        b"Serial #: 9eqzfn5j55x0d9okiui8lcwa3 "
+        b"Issued for: FERNANDO ANDRES PEREZ NAVA (fdoperez@ull.edu.es) "
+        b"Academic institution: universidad de la laguna "
+        b"Valid until: 2024-08-31 "
+        b"Issued by BayesFusion activation server"
+    ),
+    [
+        0x35,
+        0xC7,
+        0x74,
+        0xF2,
+        0x9B,
+        0x53,
+        0xC9,
+        0x68,
+        0x57,
+        0x20,
+        0x9B,
+        0x2F,
+        0x86,
+        0x54,
+        0x59,
+        0x4C,
+        0x91,
+        0xF1,
+        0xEE,
+        0x4F,
+        0xAF,
+        0x9B,
+        0xB0,
+        0x15,
+        0x28,
+        0x96,
+        0x95,
+        0x25,
+        0x08,
+        0xCA,
+        0x4C,
+        0x85,
+        0xD5,
+        0xB2,
+        0x03,
+        0xF7,
+        0x30,
+        0xB4,
+        0x47,
+        0x02,
+        0x46,
+        0x0E,
+        0xC3,
+        0xFA,
+        0x65,
+        0x7D,
+        0xBA,
+        0xA2,
+        0xF1,
+        0x98,
+        0x5E,
+        0x6C,
+        0x64,
+        0x79,
+        0x52,
+        0xAF,
+        0xB2,
+        0x95,
+        0xD4,
+        0xAC,
+        0x67,
+        0x8E,
+        0x13,
+        0xCE,
+    ],
+)
